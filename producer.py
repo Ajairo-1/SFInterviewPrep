893344422f3e1b39868b1964178aee14c0f8caa5
Ajiri Obaebor <jmo9159>
2026-09-13 19:29:10 -0500

    my own attempt at python file before getting help

--- producer.py
+++ producer.py
@@ -9,23 +9,25 @@ def delivery_report(err, msg):
     else:
         print(f"Delivered to {msg.topic()} [partition {msg.partition()}]")
 
+engineTempAsStr = ''
 config_dict = {
   "bootstrap.servers": "localhost:9092"
 }
 telemetry_reading_dict = {}
+producer = Producer(config_dict)
 for i in range(10):
     engineTempAsStr = str(random.randint(60, 95))
     time.sleep(2)
 
-telemetry_reading_dict = {
-  "equipment_Id": "12345678",
-  "engine_temperature": "67"
-}
+    telemetry_reading_dict = {
+        #?should both (k,v) pair be vars?
+      "equipment_Id": "12345678", #per execution, should auto-populate 8-digit string starting with 0000-0001
+      "engine_temperature": engineTempAsStr
+    }
 
-telem_JSON = json.dumps(telemetry_reading_dict)
+    telem_JSON = json.dumps(telemetry_reading_dict)
 
-producer = Producer(config_dict)
-producer.produce("equipment-telemetry", telem_JSON, callback=delivery_report) #callback registered
-producer.poll(0) #it only actually runs when the client processes its event queue, which happens inside poll() or flush()
-# above should be called after every produce() call to let callbacks fire incrementally
-producer.flush() #blocks scripts and ensures everything has at least failed up to this point; this should ALWAYS come last
+    producer.produce("equipment-telemetry", telem_JSON, callback=delivery_report) #callback registered
+    producer.poll(0) #it only actually runs when the client processes its event queue, which happens inside poll() or flush()
+    # above should be called after every produce() call to let callbacks fire incrementally
+producer.flush() #blocks until every queued message's callback has fired; this should ALWAYS come last
