94531e700c6508555eb024a12ae677057ac33024
Ajiri Obaebor <jmo9159>
2026-09-13 23:36:20 -0500

    next step: consumer subscribe

--- consumer.py
+++ consumer.py
@@ -6,6 +6,11 @@ Phase 3's job: Read what Phase 2's producer is publishing.
     - its config dict needs everything producer had, plus one new required key: group.id
         group id: how Kafka tracks this specific consumer's reading position, in case of crash or restart, can continue
     - pick any string for the group id ("telemetry-processor")
+
+    Subscribing
+        before a consumer can read anything, it has to explicitly tell Kafka which topic(s) it wants-- `consumer.subscribe(name_of_list)`
+        this is the API's shape.
+        A single consumer is able to subscribe to several topics at once.
 """
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 from confluent_kafka import Consumer
@@ -16,3 +21,4 @@ config_dict = {
     "group.id": groupId
 }
 consumer = Consumer(config_dict)
+consumer.subscribe("equipment-telemetry")
