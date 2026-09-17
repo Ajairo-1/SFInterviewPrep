2fde18f8d44cd825d218dc6f89cbe893cb42eb9a
Ajiri Obaebor <jmo9159>
2026-09-16 20:35:59 -0500

    created lambda_function.py file; just testing.

---
+++ lambda_function.py
@@ -0,0 +1,6 @@
+import json
+
+def lambda_handler(event, context):
+    # below code is just to prove the function runs and can see whatever data I give it
+    print(f"Event is: {event}")
+    return event
